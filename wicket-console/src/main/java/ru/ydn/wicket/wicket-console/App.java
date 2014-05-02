205e853c5f74f242631eb6814d7b324d7f226f5b
Ilya Naryzhnyy <phantom@ydn.ru>
2014-05-02 13:48:05 +0400

    Initial project commit

---
+++ wicket-console/src/main/java/ru/ydn/wicket/wicket-console/App.java
@@ -0,0 +1,13 @@
+package ru.ydn.wicket.wicket-console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
