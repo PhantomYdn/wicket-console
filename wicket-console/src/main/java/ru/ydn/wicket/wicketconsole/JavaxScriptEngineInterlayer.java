85a9334eee87cf427509d12b39f5d301c997ea72
Sergey Arabskii <barm@mail.ru>
2016-11-04 11:26:05 +0300

    added content saving by current window

--- wicket-console/src/main/java/ru/ydn/wicket/wicketconsole/JavaxScriptEngineInterlayer.java
+++ wicket-console/src/main/java/ru/ydn/wicket/wicketconsole/JavaxScriptEngineInterlayer.java
@@ -1,18 +1,29 @@
 package ru.ydn.wicket.wicketconsole;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.script.SimpleScriptContext;
 
 public class JavaxScriptEngineInterlayer implements IScriptEngineInterlayer{
 
 	private static final ScriptEngineManager manager = new ScriptEngineManager();
 
-	String name;
-	ScriptEngine engine;
+	private String name;
+	private ScriptEngine engine;
+
+	private transient ScriptContext ctx;
 
 	
 	public JavaxScriptEngineInterlayer() {
+		this.ctx = new SimpleScriptContext();
+		ctx.setWriter(new StringWriter());
+		ctx.setErrorWriter(new StringWriter());
+		ctx.setReader(new StringReader(""));
 	}
 
 	@Override
@@ -24,7 +35,15 @@ public class JavaxScriptEngineInterlayer implements IScriptEngineInterlayer{
 	public String getName() {
 		return name;
 	}
-
+	
+	private static String getContentAndClear(StringWriter writer)
+	{
+		StringBuffer buf = writer.getBuffer();
+		String ret = buf.toString();
+		buf.setLength(0);
+		return ret;
+	}
+	
 	@Override
 	public IScriptEngineInterlayerResult eval(String command) {
 		if (engine == null){
@@ -34,7 +53,9 @@ public class JavaxScriptEngineInterlayer implements IScriptEngineInterlayer{
 		JavaxScriptEngineInterlayerResult result = new JavaxScriptEngineInterlayerResult();
 		
 		try {
-			result.setReturnedObject(engine.eval(command, result.getScriptContext()));
+			result.setReturnedObject(engine.eval(command, ctx));
+			result.setOut(getContentAndClear((StringWriter)ctx.getWriter()));
+			result.setError(getContentAndClear((StringWriter)ctx.getErrorWriter()));
 			result.onUpdate();
 		} catch (ScriptException e) {
 			result.setError(e.getMessage());
--- wicket-console/src/main/java/ru/ydn/wicket/wicketconsole/JavaxScriptEngineInterlayerResult.java
+++ wicket-console/src/main/java/ru/ydn/wicket/wicketconsole/JavaxScriptEngineInterlayerResult.java
@@ -1,71 +1,9 @@
 package ru.ydn.wicket.wicketconsole;
 
-import java.io.StringReader;
-import java.io.StringWriter;
-
-import javax.script.ScriptContext;
-import javax.script.SimpleScriptContext;
-
-import org.apache.wicket.model.Model;
-
-public class JavaxScriptEngineInterlayerResult implements IScriptEngineInterlayerResult{
-	private static final long serialVersionUID = 1L;
-	
-	private transient ScriptContext ctx;
-	private transient Object result;
-	
-	private String out;
-	private String error;
-	private transient IScriptEngineInterlayerResultRenderer renderer;
-
-	public JavaxScriptEngineInterlayerResult() {
-		this.ctx = new SimpleScriptContext();
-		ctx.setWriter(new StringWriter());
-		ctx.setErrorWriter(new StringWriter());
-		ctx.setReader(new StringReader(""));
-	}
-	
-	protected ScriptContext getScriptContext(){
-		return ctx;
-	}
+public class JavaxScriptEngineInterlayerResult extends AbstractScriptEngineInterlayerResult{
 
 	@Override
-	public String getOut() {
-		return out;
-	}
-
-	protected void setOut(String out) {
-		this.out = out;
-	}
-
-	@Override
-	public String getError() {
-		return error;
-	}
-
-	protected void setError(String error) {
-		this.error = error;
-	}
-	
-	public void onUpdate(){
-		out = getContentAndClear((StringWriter)ctx.getWriter());
-		error = getContentAndClear((StringWriter)ctx.getErrorWriter()); 
-	}
-	
-	private static String getContentAndClear(StringWriter writer)
-	{
-		StringBuffer buf = writer.getBuffer();
-		String ret = buf.toString();
-		buf.setLength(0);
-		return ret;
-	}
-
-	@Override
-	public Object getReturnedObject() {
-		return result;
-	}
-
-	protected void setReturnedObject(Object result) {
-		this.result = result;
+	public void onUpdate() {
+		
 	}
 }
